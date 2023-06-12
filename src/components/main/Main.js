0bdff278aa87035a4ce1d7f7d89bf2ffee6c2f35
NickGeek1 <Nikitafilemine1@gmail.com>
2023-06-13 00:11:06 +0300

    feat: Добавил отрисовку информации о пользователе и карточек с сервера, добавил реализаицю открытия и закрытия попапов на соответствующие элементы интерфейса

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import api from '../utils/api.js';
+import api from '../../utils/Api.js';
 import Card from '../card/Card';
 
 const Main = ({onEditProfile, onEditAvatar, onAddPlace, onCardClick}) => {
